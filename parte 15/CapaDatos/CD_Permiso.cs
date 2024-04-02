1fe51cbee3adaecf57e5f0355713081369614906
FranciscoMaggi23 <fran.maggi18@gmail.com>
2024-04-02 00:39:26 -0300

    parte 15

---
+++ parte 15/CapaDatos/CD_Permiso.cs
@@ -0,0 +1,58 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Permiso
+    {
+        public List<Permiso> Listar(int idUsuario)
+        {
+            List<Permiso> lista = new List<Permiso>();
+
+            using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
+            {
+                try
+                {
+                    //sirve para hacer saltos de linea
+                    StringBuilder query = new StringBuilder();
+                    query.AppendLine("select p.IdRol, NombreMenu from PERMISO p \r\n");
+                    query.AppendLine("inner join ROL r on r.IdRol= p.IdRol\r\n");
+                    query.AppendLine("inner join USUARIO u on u.IdRol = r.IdRol\r\n");
+                    query.AppendLine("where u.IdUsuario = @idUsuario");
+
+                    SqlCommand cmd = new SqlCommand(query.ToString(), oconexion);
+                    cmd.Parameters.AddWithValue("@idUsuario", idUsuario);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(new Permiso()
+                            {
+                                oRol = new Rol() { IdRol = Convert.ToInt32(dr["IdRol"]) },
+                                NombreMenu = dr["NombreMenu"].ToString(),                             
+                            });
+                        }
+                    }
+
+
+                }
+                catch (Exception)
+                {
+
+                    lista = new List<Permiso>();
+                }
+                return lista;
+            }
+        }
+    }
+}
---
+++ parte 15/CapaPresentacion/Modales/mdProductos.cs
@@ -0,0 +1,107 @@
+﻿using CapaEntidad;
+using CapaNegocio;
+using CapaPresentacion.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaPresentacion.Modales
+{
+    public partial class mdProductos : Form
+    {
+        public Producto _Producto { get; set; }
+
+        public mdProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void mdProductos_Load(object sender, EventArgs e)
+        {
+            foreach (DataGridViewColumn columna in dgvData.Columns)
+            {
+                if (columna.Visible == true)
+                {
+                    cboBusqueda.Items.Add(new OpcionCombo() { Valor = columna.Name, Texto = columna.HeaderText });
+                }
+            }
+
+            cboBusqueda.DisplayMember = "Texto";
+            cboBusqueda.ValueMember = "Valor";
+            cboBusqueda.SelectedIndex = 0;
+
+            List<Producto> listaProducto = new CN_Producto().Listar();
+
+            foreach (Producto item in listaProducto)
+            {
+                dgvData.Rows.Add(new object[] {item.IdProducto,
+                    item.Codigo,
+                    item.Nombre,
+                    item.oCategoria.Descripcion,
+                    item.Stock,
+                    item.PrecioCompra,
+                    item.PrecioVenta,
+                });
+            }
+        }
+
+        private void dgvData_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //guardamos el indice de la fila y la columna seleccionada
+            int iRow = e.RowIndex;
+            int iColum = e.ColumnIndex;
+
+            if (iRow >= 0 && iColum > 0)
+            {
+                _Producto = new Producto()
+                {
+                    IdProducto = Convert.ToInt32(dgvData.Rows[iRow].Cells["Id"].Value.ToString()),
+                    Codigo = dgvData.Rows[iRow].Cells["Codigo"].Value.ToString(),
+                    Nombre = dgvData.Rows[iRow].Cells["Nombre"].Value.ToString(),
+                    Stock = Convert.ToInt32(dgvData.Rows[iRow].Cells["Stock"].Value.ToString()),
+                    PrecioCompra = Convert.ToDecimal(dgvData.Rows[iRow].Cells["PrecioCompra"].Value.ToString()),
+                    PrecioVenta = Convert.ToDecimal(dgvData.Rows[iRow].Cells["PrecioVenta"].Value.ToString())
+
+                };
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string columnaFiltro = ((OpcionCombo)cboBusqueda.SelectedItem).Valor.ToString();
+
+            //si existen filas en nuestra grilla
+            if (dgvData.Rows.Count > 0)
+            {
+                //recorremos cada fila de la grilla 
+                foreach (DataGridViewRow row in dgvData.Rows)
+                {
+                    //filtramos: de la columna seleccionada, retorna el valor, limpia los espacios del principio y del final,
+                    // pasalo a mayusculas, tiene que contener lo que tiene la caja de texto(esto tambien lo pasamos a mayuscula
+                    // y eliminamos espacios)
+                    if (row.Cells[columnaFiltro].Value.ToString().Trim().ToUpper().Contains(txtBusqueda.Text.Trim().ToUpper()))
+                        row.Visible = true;
+                    else
+                        row.Visible = false;
+                }
+            }
+        }
+
+        private void btnLimpiarBuscador_Click(object sender, EventArgs e)
+        {
+            txtBusqueda.Text = "";
+            foreach (DataGridViewRow row in dgvData.Rows)
+            {
+                row.Visible = true;
+            }
+        }
+    }
+}
