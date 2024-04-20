b1438472570ebe322090aed80e2bc3cd176c5070
FranciscoMaggi23 <fran.maggi18@gmail.com>
2024-04-20 13:00:50 -0300

    parte 1

---
+++ parte 1/CapaPresentacion/Frm_Usuario.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CapaPresentacion
+{
+    partial class Frm_Usuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(283, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(342, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "soy el formulario de Usuarios";
+            // 
+            // Frm_Usuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Name = "Frm_Usuario";
+            this.Text = "Frm_Usuario";
+            this.Load += new System.EventHandler(this.Frm_Usuario_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
